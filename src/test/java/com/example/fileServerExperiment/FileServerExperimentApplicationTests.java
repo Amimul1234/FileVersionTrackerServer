fa4918da0eb283319b9b04fd0d47284b00f6ab5b
Amimul <amimulahsan7@gmail.com>
2021-05-19 02:18:47 +0600

    Added rest controller.

---
+++ src/test/java/com/example/fileServerExperiment/FileServerExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileServerExperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
